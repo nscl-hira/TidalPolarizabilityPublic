Maximum mass bug calculation fixed

TidalLove/TidalLoveWrapper.py
@@ -39,7 +39,7 @@ class TidalLoveWrapper:
         mass, radius, lambda_, checkpoint_mass, checkpoint_radius = tidal.tidallove_individual(self.output.name, pc, self.checkpoint)
         return mass, radius, lambda_, checkpoint_mass, checkpoint_radius
 
-    def FindMaxMass(self, central_pressure0=30, disp=False, *args):
+    def FindMaxMass(self, central_pressure0=300, disp=False, *args):
         # try finding the maximum mass
         pc = np.nan
         try:
@@ -49,7 +49,7 @@ class TidalLoveWrapper:
             max_mass = np.nan
         return max_mass, pc
 
-    def FindMass14(self, central_pressure0=30, *args):
+    def FindMass14(self, central_pressure0=300, *args):
         try:
             pc = opt.newton(self._GetMass14, x0=central_pressure0, *args)
             mass, radius, lambda_, checkpoint_mass, checkpoint_radius = self.Calculate(pc)

Utilities/SkyrmeEOS.py
@@ -74,14 +74,14 @@ class EOSSpline:
             self.SymSpl = UnivariateSpline(rho_Sym, Sym, s=smooth_sym)
             self.dSymSpl = self.SymSpl.derivative(1)
             self.ddSymSpl = self.SymSpl.derivative(2)
+
+        self.sound = None
         if pressure is None:
             self.SplPressure = None
         else:
             self.SplPressure = UnivariateSpline(rho, pressure, s=0)
             if energy_density is not None:
                 self.sound = UnivariateSpline(energy_density, pressure, s=smooth)
-            else:
-                self.sound = None
         #plt.plot(rho, energy, 'ro')
         #plt.plot(0.16*np.linspace(0.1, 3, 100), self.spl(0.16*np.linspace(0.1, 3, 100)))
         #plt.plot(0.16*np.linspace(0.1, 3, 100), self.dSymSpl(0.16*np.linspace(0.1, 3, 100)))