Bug fixed on deformability radius calculation

TidalLove/TidalLoveWrapper.py
@@ -35,7 +35,7 @@ class TidalLoveWrapper:
         # pressure needs to be expressed as pascal for pc
         self.max_pressure# /= 3.62704e-5
         self.ans = ()
-        self.surface_pressure = 1e-8 # default pressure defined at surface
+        self.surface_pressure = 1e-11 # default pressure defined at surface
         self.checkpoint = [1e-3]
 
     def __enter__(self):
@@ -50,7 +50,7 @@ class TidalLoveWrapper:
         self.ans = tidal.tidallove_individual(self.output.name, 
                                               pc, self.max_energy, self.surface_pressure, np.array(self.checkpoint), )
         if(len(self.ans[4]) > 0):
-            self.ans = (self.ans[0], self.ans[4][-1], self.ans[2], self.ans[3], self.ans[4])
+            self.ans = (self.ans[0], self.ans[1], self.ans[2], self.ans[3], self.ans[4])
         if self.ans[0] < 0:
             raise RuntimeError('Calculated mass smaller than zero. EOS exceed its valid range')
         return self.ans